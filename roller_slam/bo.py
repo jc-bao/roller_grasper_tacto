e81ab07eec79254400789532a6953701c25220aa
jc-bao <jc-bao@outlook.com>
2022-09-01 15:39:54 +0800

    finish the whole pipeline

--- roller_slam/bo.py
+++ roller_slam/bo.py
@@ -24,7 +24,7 @@ def eval_projection(projection_shape, hole_shape):
   return False
 
 def eval_shape(sec_mu, sec_var):
-  x_min = torch.linspace(-2, 2, 11).unsqueeze(-1)
+  x_min = torch.linspace(-0.4, 0.4, 11).unsqueeze(-1)
   dist = torch.tensor(sec_mu)
   dist_std = torch.sqrt(torch.tensor(sec_var))
   x_normed = (x_min - dist) / dist_std # Tensor(n_x, n_theta)
@@ -32,7 +32,7 @@ def eval_shape(sec_mu, sec_var):
   mask = log_cdf > -5
   cdf = torch.zeros_like(log_cdf)
   cdf[mask] = torch.exp(log_cdf[mask])
-  cdf = torch.prod(1 - cdf, dim=1) # Tensor(n_x)
+  cdf = torch.prod(1 - cdf, dim=1) # Tensor(n_x
   pdf = cdf[:-1] - cdf[1:]
   pdf = pdf / pdf.sum()
   mean = torch.sum(pdf * x_min[:-1,0])
@@ -42,16 +42,16 @@ def eval_shape(sec_mu, sec_var):
 def eval_section(y_mu, y_var, hole_shape_polar):
   assert y_mu.shape[0] == hole_shape_polar.shape[
       0], "The number of points in the hole shape and the Gaussian Process must be the same"
-  x_min = torch.linspace(-2, 2, 101).unsqueeze(-1)
+  x_min = torch.linspace(-0.4, 0.4, 101).unsqueeze(-1)
   dist = torch.tensor(hole_shape_polar[:,1] - y_mu)
   dist_std = torch.sqrt(torch.tensor(y_var))
   x_normed = (x_min - dist) / dist_std # Tensor(n_x, n_theta)
   log_cdf = gpytorch.log_normal_cdf(x_normed)
   mask = log_cdf > -5
-  cdf = torch.zeros_like(log_cdf)
+  cdf = torch.zeros_like(log_cdf) # probability of x_min < X
   cdf[mask] = torch.exp(log_cdf[mask])
-  cdf = torch.prod(1 - cdf, dim=1) # Tensor(n_x)
-  pdf = cdf[:-1] - cdf[1:]
+  cdf = torch.prod(1 - cdf, dim=1) # Tensor(n_x) p(x_min > X)
+  pdf = cdf[:-1] - cdf[1:] 
   pdf = pdf / pdf.sum()
   mean = torch.sum(pdf * x_min[:-1,0])
   var = torch.sum(pdf * ((x_min[:-1,0] - mean)**2))
@@ -65,18 +65,28 @@ def main():
   # object SLAM parameters
   n_phi = 10
   n_theta = n_phi * 2
+  section_width = 0.05
   angle_step = torch.pi/n_phi
 
   # num_plts = 3
   # fig = plt.figure(figsize=(5, 5*num_plts))
-  plotter = Plotter(num_figs=32)
+  plotter = Plotter(num_figs=48)
   # load the data
-  points_normed = load_data('../test/assets/bottle.ply')
-  plotter.plot_3d([points_normed], 'bottle point clouds')
+  points = load_data('../test/assets/bottle.ply')
+  xxyy = np.stack(np.meshgrid(np.linspace(-0.05, 0.05, 8), np.linspace(-0.05, 0.05, 8)), axis=-1)
+  ttrr = cartisian_to_polar(xxyy)
+  mask = ttrr[...,1] < (0.03/np.sqrt(1-0.75*(np.cos(ttrr[...,0]+np.pi/4)**2)))
+  extra_point = xxyy[mask]
+  extra_point = np.concatenate([extra_point, np.zeros((*extra_point.shape[:-1], 1))], axis=-1)
+  points = np.concatenate([points, extra_point], axis=0)
+  points_mean = np.mean(points, axis=0)
+  points_std = np.std(points, axis=0)
+  points_normed = (points - points_mean) / points_std
+  plotter.plot_3d([points], 'normed bottle point clouds', true_aspect=True)
 
   # get the hole shape
   hole_euler = np.array([0, 0, 0])
-  hole_shape = get_hole_shape(points_normed, R.from_euler('xyz', hole_euler))
+  hole_shape = get_hole_shape(points, R.from_euler('xyz', hole_euler))
   hole_shape_polar = cartisian_to_polar(hole_shape)
   # sort array
   hole_shape_polar = hole_shape_polar[hole_shape_polar[:, 0].argsort()]
@@ -97,78 +107,93 @@ def main():
   min_dist_var = []
   for explore_step in range(5):
     # explore the object
-    section_points = get_section_points(points_normed, explore_section_pos, section_width=0.5)
-    plotter.plot_3d([section_points], f'section points, orn={explore_section_pos}')
+    section_points = get_section_points(points, explore_section_pos, section_width)
+    plotter.plot_3d([section_points], f'section points, orn={explore_section_pos}', true_aspect=True)
 
     # fit a probabilistic model to the section points
-    section_point_spherical = cartisian_to_spherical(section_points)
-    section_point_tensor = torch.tensor(section_point_spherical)
-    gp_shape = GaussianProcess(section_point_tensor[:,:2], section_point_tensor[:,2], train_num=100)
+    normed_section_points = (section_points - points_mean) / points_std
+    normed_section_point_spherical = cartisian_to_spherical(normed_section_points)
+    normed_section_point_tensor = torch.tensor(normed_section_point_spherical)
+    gp_shape = GaussianProcess(normed_section_point_tensor[:,:2], normed_section_point_tensor[:,2], train_num=100)
     x_pred, y_pred_mu, y_pred_var = gp_shape.predict(step=angle_step)
-    pred_point_spherical = torch.cat((x_pred, y_pred_mu.unsqueeze(-1)), dim=-1).detach().numpy()
-    pred_point = spherical_to_cartisian(pred_point_spherical)
-    pred_var = y_pred_var.detach().numpy()
-    plotter.plot_3d([pred_point.reshape(-1,3)], 'gp shape', c=pred_var.flatten())
+    normed_pred_point_spherical = torch.cat((x_pred, y_pred_mu.unsqueeze(-1)), dim=-1).detach().numpy()
+    normed_pred_point = spherical_to_cartisian(normed_pred_point_spherical)
+    normed_pred_var = y_pred_var.detach().numpy()
+    pred_point = normed_pred_point * points_std + points_mean
+    pred_point_spherical = cartisian_to_spherical(pred_point).reshape(n_theta, n_phi, 3)
+    pred_point_orn = R.from_euler('zy', pred_point_spherical[:,:,:2].reshape(-1,2))
+    pred_var = normed_pred_var.reshape(n_theta, n_phi)
+    pred_point_rot_vec = pred_point_orn.apply(np.array([0,0,1])).reshape(n_theta, n_phi, 3)
+    pred_point_std = np.dot(pred_point_rot_vec, points_std)
+    pred_var = pred_var * (pred_point_std**2)
+    plotter.plot_3d([pred_point.reshape(-1,3)], 'gp shape', c=pred_var.flatten(), true_aspect=True)
 
     # evaluate the shape
     # enumerate all possible orientations
-    theta = np.arange(-np.pi, np.pi, angle_step)
-    phi = np.arange(-np.pi/2, np.pi/2, angle_step)
-    all_ori = np.stack(np.meshgrid(theta, phi), axis=-1).reshape(-1, 2)
-    y_pred_mu_new = y_pred_mu.reshape(n_theta, n_phi).detach().numpy()
-    y_pred_var_new = y_pred_var.reshape(n_theta, n_phi).detach().numpy()
     # try different object orientations
-    sec_err_mu = np.zeros((n_theta*n_phi, n_phi)) # ndarray(n_orientation, n_section)
-    sec_err_var = np.zeros((n_theta*n_phi, n_phi))
-    obj_err_mu = np.zeros(n_theta*n_phi) 
-    obj_err_var = np.zeros(n_theta*n_phi)
-    for i, ori in enumerate(all_ori):
-      x_pred_new = (x_pred + ori).reshape(n_theta, n_phi, 2).detach().numpy()
-      x_pred_new[:,:,0] = (x_pred_new[:,:,0]+np.pi) % (2 * np.pi) - np.pi
-      x_pred_new[:,:,1] = (x_pred_new[:,:,1]+np.pi/2) % np.pi - np.pi/2
-      theta_argsort = np.argsort(x_pred_new[:,:,0], axis=0)
-      phi_argsort = np.argsort(x_pred_new[:,:,1], axis=1)
-      y_pred_mu_ori = y_pred_mu_new[theta_argsort, phi_argsort]
-      y_pred_var_ori = y_pred_var_new[theta_argsort, phi_argsort]
-      # evaluate different sections
-      for section_phi_idx in range(n_phi):
-        section_y_mu = y_pred_mu_ori[:, section_phi_idx] * np.cos(phi[section_phi_idx])
-        section_y_var = y_pred_var_ori[:, section_phi_idx] * (np.cos(phi[section_phi_idx])**2)
-        err_mu, err_var = eval_section(section_y_mu, section_y_var, hole_shape_polar)
-        sec_err_mu[i, section_phi_idx] = err_mu
-        sec_err_var[i, section_phi_idx] = err_var
-      obj_mu, obj_var = eval_shape(sec_err_mu[i], sec_err_var[i])
-      obj_err_mu[i] = obj_mu
-      obj_err_var[i] = obj_var
-    data = np.concatenate([all_ori, np.expand_dims(obj_err_mu, axis=1)], axis=1)
-    mll_ori_idx = np.argmax(obj_err_mu)
+    sec_starts = np.zeros((n_theta, n_phi, 20))
+    sec_err_mu = -np.ones((n_theta, n_phi, 20)) # ndarray(n_orientation, n_section)
+    sec_err_var = (1e-6)*np.ones((n_theta, n_phi, 20))
+    obj_err_mu = np.zeros((n_theta, n_phi)) 
+    obj_err_var = np.zeros((n_theta, n_phi))
+    for i in range(n_theta):
+      for j in range(n_phi):
+        rot = R.from_euler('zy', -pred_point_spherical[i,j,:2])
+        pred_point_rot = (rot.apply(pred_point)).reshape(n_theta, n_phi, 3)
+        pred_point_spherical_rot = cartisian_to_spherical(pred_point_rot)
+        pred_point_rot = spherical_to_cartisian(pred_point_spherical_rot)
+        # evaluate different sections
+        for section_phi_idx, section_start in enumerate(np.arange(np.min(pred_point_rot[:,:,2]), np.max(pred_point_rot[:,:,2]), section_width)):
+          sec_starts[i,j,section_phi_idx] = section_start
+          section_end = section_start + section_width
+          mask = (pred_point_rot[:,:,2] > section_start) & (pred_point_rot[:,:,2] < section_end)
+          phi = pred_point_spherical_rot[mask, 1]
+          theta = pred_point_spherical_rot[mask, 0]
+          section_mu = pred_point_spherical_rot[mask, 2] * np.cos(phi)
+          section_var = pred_var[mask] * (np.cos(phi)**2)
+          hole_shape_polar= np.stack([theta, hole_shape_interp(theta)], axis=1)
+          err_mu, err_var = eval_section(section_mu, section_var, hole_shape_polar)
+          sec_err_mu[i, j, section_phi_idx] = err_mu
+          sec_err_var[i, j, section_phi_idx] = err_var
+        obj_mask = sec_err_var[i,j,:] > 1e-5
+        obj_mu, obj_var = eval_shape(sec_err_mu[i,j][obj_mask], sec_err_var[i,j][obj_mask])
+        obj_err_mu[i, j] = obj_mu
+        obj_err_var[i, j] = obj_var
+    all_ori = pred_point_spherical[:,:,:2]
+    data = np.concatenate([all_ori, np.expand_dims(obj_err_mu, axis=-1)], axis=-1)
+    mll_ori_idx = np.unravel_index(np.argmax(obj_err_mu), obj_err_mu.shape)
     mll_ori = all_ori[mll_ori_idx]
     mll_ori_err_mu = obj_err_mu[mll_ori_idx]
     mll_ori_err_var = obj_err_var[mll_ori_idx]
     min_dist_mu.append(mll_ori_err_mu)
     min_dist_var.append(mll_ori_err_var)
-    plotter.plot_3d([data], f'object errors, \n best_ori={mll_ori}, \n mu={mll_ori_err_mu:.2f}, \n var={mll_ori_err_var:.2f}', c=obj_err_var, axis_name=['theta', 'phi', 'margin'])
+    plotter.plot_3d([data.reshape(-1,3)], f'object errors, \n best_ori={mll_ori}, \n mu={mll_ori_err_mu:.2f}, \n var={mll_ori_err_var:.2f}', c=obj_err_var.flatten(), axis_name=['theta', 'phi', 'margin'])
 
     # surrogate function
     obj_aq = obj_err_mu + obj_err_var * 10
-    max_pos = np.argmax(obj_aq)
+    max_pos = np.unravel_index(np.argmax(obj_aq), obj_aq.shape)
     best_ori = all_ori[max_pos]
-    data = np.concatenate([all_ori, np.expand_dims(obj_aq, axis=1)], axis=1)
-    plotter.plot_3d([data, data[[max_pos]]], f'aquisition function, \n best_ori={best_ori}', axis_name=['theta', 'phi', 'aq_value'])
+    data = np.concatenate([all_ori, np.expand_dims(obj_aq, axis=-1)], axis=-1)
+    plotter.plot_3d([data.reshape(-1,3), data[max_pos].reshape(-1,3)], f'aquisition function, \n best_ori={best_ori}', axis_name=['theta', 'phi', 'aq_value'])
 
     # evaluate the section
     best_sec_err_mu = sec_err_mu[max_pos]
     best_sec_err_var = sec_err_var[max_pos]
-    data = np.stack([phi, -best_sec_err_mu], axis=1)
-    plotter.plot_2d([data], f'-section errors', c=best_sec_err_var, axis_name=['phi', 'margin'])
+    best_sec_start = sec_starts[max_pos]
+    best_sec_mask = best_sec_err_var > 1e-5
+    best_sec_err_mu = best_sec_err_mu[best_sec_mask]
+    best_sec_err_var = best_sec_err_var[best_sec_mask]
+    best_sec_range = best_sec_start[best_sec_mask] + section_width/2
+    data = np.stack([best_sec_range, -best_sec_err_mu], axis=1)
+    plotter.plot_2d([data], f'-section errors', c=best_sec_err_var, axis_name=['section displacement', 'margin'])
     sec_aq = -best_sec_err_mu + best_sec_err_var * 10
     max_pos = np.argmax(sec_aq)
-    best_sec_phi = phi[max_pos]
-    data = np.stack([phi, sec_aq], axis=1)
-    plotter.plot_2d([data, data[[max_pos]]], f'section aquisition function, \n best_sec_phi={best_sec_phi}', axis_name=['phi', 'aq_value'])
+    best_sec_disp = best_sec_range[max_pos]
+    data = np.stack([best_sec_range, sec_aq], axis=1)
+    plotter.plot_2d([data, data[[max_pos]]], f'section aquisition function, \n best_sec_phi={best_sec_disp}', axis_name=['phi', 'aq_value'])
 
     # next exploration part
-    explore_section_pos.append(np.append(best_ori, np.mean(best_sec_err_mu)*np.sin(best_sec_phi)))
+    explore_section_pos.append(np.append(best_ori, best_sec_disp))
   data = np.stack((np.arange(len(min_dist_mu)), min_dist_mu), axis=-1)
   plotter.plot_2d([data], title='error change over time', c=np.array(min_dist_var))
 
--- roller_slam/utils.py
+++ roller_slam/utils.py
@@ -6,12 +6,13 @@ from scipy.spatial.transform import Rotation as R
 from scipy.spatial import ConvexHull
 from tqdm import trange
 import open3d as o3d
+from scipy.optimize import curve_fit
 
 def cartisian_to_spherical(points):
   points_spherical = np.zeros(points.shape)
   points_spherical[..., 0] = np.arctan2(points[..., 1], points[..., 0])
-  points_spherical[..., 1] = np.arctan2(points[...,2], np.linalg.norm(points[...,:2], axis=1))
-  points_spherical[..., 2] = np.linalg.norm(points, axis=1)
+  points_spherical[..., 1] = np.arctan2(points[...,2], np.linalg.norm(points[...,:2], axis=-1))
+  points_spherical[..., 2] = np.linalg.norm(points, axis=-1)
   return points_spherical
 
 def spherical_to_cartisian(points_spherical):
@@ -24,7 +25,7 @@ def spherical_to_cartisian(points_spherical):
 def cartisian_to_polar(points):
   points_polar = np.zeros(points.shape)
   points_polar[..., 0] = np.arctan2(points[..., 1], points[..., 0])
-  points_polar[..., 1] = np.linalg.norm(points[..., :2], axis=1)
+  points_polar[..., 1] = np.linalg.norm(points[..., :2], axis=-1)
   return points_polar
 
 class Plotter():
@@ -59,7 +60,7 @@ class Plotter():
       plt.colorbar(sc)
     ax.set_title(title)
 
-  def plot_3d(self, datas, title, c = None, axis_name = ['x', 'y', 'z']):
+  def plot_3d(self, datas, title, c = None, axis_name = ['x', 'y', 'z'], true_aspect=False):
     ax = self.fig.add_subplot(self.num_figs,1,self.current_fig_id, projection='3d')
     self.current_fig_id += 1
     for data in datas:
@@ -73,7 +74,39 @@ class Plotter():
     ax.set_ylabel(axis_name[1])
     ax.set_zlabel(axis_name[2])
     ax.set_title(title)
-    # ax.set_box_aspect((np.ptp(data[:,0]), np.ptp(data[:,1]), np.ptp(data[:,2])))
+    if true_aspect:
+      ax.set_box_aspect((np.ptp(data[:,0]), np.ptp(data[:,1]), np.ptp(data[:,2])))
+
+  def plot_3d_surface(self, datas, title, c = None, axis_name = ['x', 'y', 'z'], true_aspect=False):
+    ax = self.fig.add_subplot(self.num_figs,1,self.current_fig_id, projection='3d')
+    def function(data, a, b, c, d, e, f, g, h, i, j, k):
+      x = data[..., 0]
+      y = data[..., 1]
+      return a+b*x+c*y+d*x*x+e*y*y+f*x*x*x+g*y*y*y+h*x*x*x*x+i*y*y*y*y+j*x*x*x*x*x+k*y*y*y*y*y
+    for data in datas:
+      model_x_data = np.linspace(min(data[:,0]), max(data[:,1]), 100)
+      model_y_data = np.linspace(min(data[:,0]), max(data[:,1]), 100)
+      # create coordinate arrays for vectorized evaluations
+      X, Y = np.meshgrid(model_x_data, model_y_data)
+      XY = np.stack([X, Y], axis=2)
+      # calculate Z coordinate array
+      parameters, covariance = curve_fit(function, data[:,:2], data[:,2])
+      Z = function(XY, *parameters)
+      self.current_fig_id += 1
+      if c is None:
+        ax.plot_surface(X, Y, Z)
+      else:
+        # cm = plt.cm.get_cmap('viridis')
+        parameters, covariance = curve_fit(function, data[:,:2], c)
+        C = function(XY, *parameters)
+        sc = ax.plot_surface(X, Y, Z, facecolors=plt.cm.jet(C))
+        plt.colorbar(sc)
+    ax.set_xlabel(axis_name[0])
+    ax.set_ylabel(axis_name[1])
+    ax.set_zlabel(axis_name[2])
+    ax.set_title(title)
+    if true_aspect:
+      ax.set_box_aspect((np.ptp(data[:,0]), np.ptp(data[:,1]), np.ptp(data[:,2])))
 
   def save(self, path):
     self.fig.savefig(path)
@@ -84,9 +117,7 @@ def load_data(path, voxel_size=0.02):
   origin_pcd = o3d.io.read_point_cloud(path)
   pcd = origin_pcd.voxel_down_sample(voxel_size=voxel_size)
   points = np.asarray(pcd.points)
-  mean = np.mean(points, axis=0)
-  std = np.std(points, axis=0)
-  return (points - mean) / std
+  return points
 
 def get_hole_shape(points, orn:R):
   points_transformed = orn.apply(points)
@@ -95,16 +126,8 @@ def get_hole_shape(points, orn:R):
   hull = ConvexHull(points_projected)
   return points_projected[hull.vertices]
 
-def get_section_points(points, section_poses, section_width = 0.8):
-  # theta = np.arange(-np.pi, np.pi, np.pi/15)
-  # phi = np.arange(-np.pi/2, np.pi/2, np.pi/15)
-  # base_ori = np.stack(np.meshgrid(theta, phi), axis=-1).reshape(-1, 2)
-  # shape = np.ones((base_ori.shape[0], 3)) * 1.5
-  # shape[:, :2] = base_ori
-  # shape += np.random.normal(0, 0.4, shape.shape)
-  # shape = spherical_to_cartisian(shape)
+def get_section_points(points, section_poses, section_width = 0.05):
   mask_in = np.zeros(points.shape[0], dtype=bool)
-  # mask_out = np.ones(shape.shape[0], dtype=bool)
   for section_pos in section_poses:
     orn = R.from_euler('xyz', np.append(section_pos[:2],0))
     disp = section_pos[2]
@@ -112,11 +135,7 @@ def get_section_points(points, section_poses, section_width = 0.8):
     z_end = disp + section_width/2
     points_transformed = orn.apply(points)
     mask_in |= ((points_transformed[:, 2] > z_start) & (points_transformed[:, 2] < z_end))
-    # mask_out &= ((shape[:, 2] < (z_start-1.0)) | (shape[:, 2] > (z_end+1.0)))
-  return np.concatenate([
-    points[mask_in], 
-    # shape[mask_out],
-  ],axis=0)
+  return points[mask_in]
 
 '''probabilistic model'''
 # We will use the simplest form of GP model, exact inference
@@ -125,6 +144,8 @@ class ExactGPModel(gpytorch.models.ExactGP):
     super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
     self.mean_module = gpytorch.means.ConstantMean()
     self.covar_module = gpytorch.kernels.ScaleKernel(
+      gpytorch.kernels.RBFKernel()) + gpytorch.kernels.ScaleKernel(
+      gpytorch.kernels.RBFKernel()) + gpytorch.kernels.ScaleKernel(
       gpytorch.kernels.RBFKernel())
 
   def forward(self, x):
@@ -142,10 +163,11 @@ class GaussianProcess():
       train_y_full.append(train_y)
     train_x = torch.cat(train_x_full, dim=0)
     train_y = torch.cat(train_y_full, dim=0)
-    self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    self.likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(noise=torch.tensor([0.0]))
     self.model = ExactGPModel(train_x, train_y, self.likelihood)
     hypers = {
-      'covar_module.base_kernel.lengthscale': torch.tensor(0.572),
+      'covar_module.kernels.0.base_kernel.lengthscale': torch.tensor(0.4),
+      'covar_module.kernels.1.base_kernel.lengthscale': torch.tensor(0.02),
     }
     self.model_params = self.model.initialize(**hypers)
 
@@ -160,7 +182,7 @@ class GaussianProcess():
       output = self.model(train_x)
       loss = -mll(output, train_y)
       loss.backward()
-      pbar.set_description(f'Iter {i+1}/{train_num} - Loss: {loss.item():.3f}, lengthscale: {self.model.covar_module.base_kernel.lengthscale.item():.3f}')
+      pbar.set_description(f'Iter {i+1}/{train_num} - Loss: {loss.item():.3f}, lengthscale0: {self.model.covar_module.kernels[0].base_kernel.lengthscale.item():.3f}, lengthscale1: {self.model.covar_module.kernels[1].base_kernel.lengthscale.item():.3f}')
       optimizer.step()
 
   def predict(self, step = np.pi/60):
