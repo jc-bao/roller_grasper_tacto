02080ee713b15d4425e00960b973cff8a6190f15
jc-bao~ <jc-bao@outlook.com>
2022-09-05 15:21:34 +0800

    update experiment manager

--- roller_slam/main.py
+++ roller_slam/main.py
@@ -1,18 +1,34 @@
 from roller_slam.bo import run_bo
 import ray
 import numpy as np
+import pandas as pd
 
 @ray.remote
-def main(init_explore_section):
-  return run_bo(init_explore_section)
+def main(x):
+  init_explore_section = x[:3]
+  explore_policy = 'bo'
+  UCB_alpha = x[3]
+  return run_bo(init_explore_section, explore_policy, UCB_alpha)
   
 
 if __name__ == '__main__':
+  UCB_alpha = np.array([500])
+  theta = np.array([0])
   phi = np.arange(-np.pi/2, np.pi/2, np.pi/4)
-  all_explore_values = np.zeros((phi.shape[0], 3))
-  all_explore_values[:,1] = phi
-  results = [main.remote(x) for x in all_explore_values]
+  section_disp = np.array([0])
+  xx = np.stack(np.meshgrid(theta, phi, section_disp, UCB_alpha), axis=-1)
+  xx_flatten = xx.reshape(-1, xx.shape[-1])
+  results = [main.remote(x) for x in xx_flatten]
   results = np.array(ray.get(results))
   err = results[:, 0]
   step = results[:, 1]
+  data = {
+    'theta': xx_flatten[..., 0], 
+    'phi': xx_flatten[..., 1], 
+    'disp': xx_flatten[..., 2], 
+    'err': err, 
+    'step': step
+  }
+  data = pd.DataFrame(data)
+  data.save_csv('../test/results/data.csv')
   print(err, step)
